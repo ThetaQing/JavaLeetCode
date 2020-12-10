43dcc42864dda5fcc789413c6b640eba44305bd8
ThetaQing <csusq2016@163.com>
2020-12-10 21:47:45 +0800

    贪心算法-利用ArrayList.add(ind,object)的思想对数组重新排序

---
+++ src/main/java/leetcode/editor/cn/GreedyAlgorithm/根据身高重建队列.java
@@ -0,0 +1,94 @@
+package main.java.leetcode.editor.cn.GreedyAlgorithm;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+
+/**
+假设有打乱顺序的一群人站成一个队列，数组 people 表示队列中一些人的属性（不一定按顺序）。每个 people[i] = [hi, ki] 表示第 i
+个人的身高为 hi ，前面 正好 有 ki 个身高大于或等于 hi 的人。
+
+ 请你重新构造并返回输入数组 people 所表示的队列。返回的队列应该格式化为数组 queue ，其中 queue[j] = [hj, kj] 是队列中第
+ j 个人的属性（queue[0] 是排在队列前面的人）。
+
+
+
+
+
+
+ 示例 1：
+
+
+输入：people = [[7,0],[4,4],[7,1],[5,0],[6,1],[5,2]]
+输出：[[5,0],[7,0],[5,2],[6,1],[4,4],[7,1]]
+解释：
+编号为 0 的人身高为 5 ，没有身高更高或者相同的人排在他前面。
+编号为 1 的人身高为 7 ，没有身高更高或者相同的人排在他前面。
+编号为 2 的人身高为 5 ，有 2 个身高更高或者相同的人排在他前面，即编号为 0 和 1 的人。
+编号为 3 的人身高为 6 ，有 1 个身高更高或者相同的人排在他前面，即编号为 1 的人。
+编号为 4 的人身高为 4 ，有 4 个身高更高或者相同的人排在他前面，即编号为 0、1、2、3 的人。
+编号为 5 的人身高为 7 ，有 1 个身高更高或者相同的人排在他前面，即编号为 1 的人。
+因此 [[5,0],[7,0],[5,2],[6,1],[4,4],[7,1]] 是重新构造后的队列。
+
+
+ 示例 2：
+
+
+输入：people = [[6,0],[5,0],[4,0],[3,2],[2,2],[1,4]]
+输出：[[4,0],[5,0],[2,2],[3,2],[1,4],[6,0]]
+
+
+
+
+ 提示：
+
+
+ 1 <= people.length <= 2000
+ 0 <= hi <= 106
+ 0 <= ki < people.length
+ 题目数据确保队列可以被重建
+
+ Related Topics 贪心算法
+ 👍 711 👎 0
+
+ 为了使插入操作不影响后续的操作，身高较高的学生应该先做插入操作，否则身高较小的学生原先正确插入的第 k 个位置可能会变成第 k+1 个位置。
+
+ 身高 h 降序、个数 k 值升序，然后将某个学生插入队列的第 k 个位置中。
+
+ 不断得到局部最优解
+
+*/
+public class 根据身高重建队列{
+    public static void main(String[] args){
+        Solution solution = new 根据身高重建队列().new Solution();
+        int[][] arr = {{7,0}, {4,4}, {7,1}, {5,0}, {6,1}, {5,2}};
+        int[][] ints = solution.reconstructQueue(arr);
+
+
+        System.out.println(ints);
+    }
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int[][] reconstructQueue(int[][] people) {
+        if(people==null || people.length == 0 || people[0].length==0){
+            return new int[0][0];
+        }
+
+        Arrays.sort(people, new Comparator<int[]>() {
+           @Override
+           public int compare(int[] o1, int[] o2) {
+               return o1[0]==o2[0]?o1[1]-o2[1]:o2[0]-o1[0];
+           }
+       });
+
+        ArrayList<int[]> queue = new ArrayList<>();
+        for (int[] person : people) {
+            queue.add(person[1],person);
+        }
+               return queue.toArray(new int[queue.size()][]);
+
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
