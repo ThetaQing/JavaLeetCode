7bc217f75434346b696498d2c4a895994913aaf2
ThetaQing <csusq2016@163.com>
2020-12-12 09:54:52 +0800

    贪心算法-确定修改的数及值

---
+++ src/main/java/leetcode/editor/cn/GreedyAlgorithm/非递减数列.java
@@ -0,0 +1,66 @@
+package main.java.leetcode.editor.cn.GreedyAlgorithm;
+/**
+给你一个长度为 n 的整数数组，请你判断在 最多 改变 1 个元素的情况下，该数组能否变成一个非递减数列。
+
+ 我们是这样定义一个非递减数列的： 对于数组中所有的 i (0 <= i <= n-2)，总满足 nums[i] <= nums[i + 1]。
+
+
+
+ 示例 1:
+
+ 输入: nums = [4,2,3]
+输出: true
+解释: 你可以通过把第一个4变成1来使得它成为一个非递减数列。
+
+
+ 示例 2:
+
+ 输入: nums = [4,2,1]
+输出: false
+解释: 你不能在只改变一个元素的情况下将其变为非递减数列。
+
+
+
+
+ 说明：
+
+
+ 1 <= n <= 10 ^ 4
+ - 10 ^ 5 <= nums[i] <= 10 ^ 5
+
+ Related Topics 数组
+ 👍 382 👎 0
+
+ 有修改要求的要确定什么时候修改，如何修改！！！
+
+ 在出现 nums[i] < nums[i - 1] 时，需要考虑的是应该修改数组的哪个数，使得本次修改能使 i 之前的数组成为非递减数组，
+ 并且 不影响后续的操作 。优先考虑令 nums[i - 1] = nums[i]，因为如果修改 nums[i] = nums[i - 1] 的话，
+ 那么 nums[i] 这个数会变大，就有可能比 nums[i + 1] 大，从而影响了后续操作。还有一个比较特别的情况就是 nums[i] < nums[i - 2]，
+ 修改 nums[i - 1] = nums[i] 不能使数组成为非递减数组，只能修改 nums[i] = nums[i - 1]。
+*/
+public class 非递减数列{
+    public static void main(String[] args){
+        Solution solution = new 非递减数列().new Solution();
+    }
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public boolean checkPossibility(int[] nums) {
+        int cnt=0;
+        for (int i = 1; i < nums.length && cnt < 2; i++) {
+            if(nums[i] >= nums[i-1]){
+                continue;
+            }
+            cnt++;
+            if(i-2>=0 && nums[i] < nums[i-2]){
+                nums[i] = nums[i-1];
+            }else{
+                nums[i-1] = nums[i];
+            }
+        }
+        return cnt<=1;
+
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
